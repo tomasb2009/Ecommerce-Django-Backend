e9b4bfa272f9e460a7002c65caffaaae8c615bff
tomasb2009 <t.barragan@itsv.edu.ar>
2026-02-25 22:34:52 -0300

    SEVENTH commit

--- backend/settings.py
+++ backend/settings.py
@@ -113,7 +113,7 @@ DATABASES = {
     }
 }
 
-db_from_env=dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=0)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
